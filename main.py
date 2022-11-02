f04213cf2287f31cdc36c496c0efa185569ea05c
Vilkasek <jakub.brandt2002@gmail.com>
2022-11-02 12:07:07 +0100

    Dodanie pliku do repo

---
+++ main.py
@@ -0,0 +1,40 @@
+def main():
+    name = str(input("Podaj imie: "))
+    name2 = str(input("Podaj imie: "))
+    
+    n = int(input("Podaj liczbe: "))
+
+    suma = 0
+
+    zadanie_pierwsze(name)
+    zadanie_drugie(name2)
+
+    zadanie_trzecie_for(n, suma)
+    zadanie_trzecie_while(n, suma)
+
+def zadanie_pierwsze(name):
+    print("Zadanie pierwsze:\n")
+
+    print("Witaj "+ name)
+
+def zadanie_drugie(name2):
+    print("Zadanie drugie:\n")
+
+    if name2 == "Anna" or name2 == "Jan":
+        print("Witaj " + name2)
+    else:
+        print("Witaj")
+
+def zadanie_trzecie_for(n, suma):
+    for i in range(1, n+1):
+        suma = suma + i
+    
+    print(suma)
+
+def zadanie_trzecie_while(n, suma):
+    i = 0
+    while i < n:
+        i = i + 1
+        print(i)
+
+__name__ = main()
