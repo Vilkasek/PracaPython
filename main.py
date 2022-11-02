c44a63685026ef528452b6774f4ce011b924b301
Vilkasek <jakub.brandt2002@gmail.com>
2022-11-02 14:13:51 +0100

    Proba wykonania zadania siodmego

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ def main():
     zadanie_trzecie_while(suma)
     zadanie_czwarte(suma)
     zadanie_piate(suma, iloczyn)
+    zadanie_szoste()
+    zadanie_siodme()
 
 def zadanie_pierwsze(name):
     print("Zadanie pierwsze:\n")
@@ -74,6 +76,17 @@ def zadanie_piate(suma, iloczyn):
         for i in range(1, n+1):
             iloczyn = iloczyn * i
         print(iloczyn)
+
+# def zadanie_szoste():
+#     pass
+#
+# def zadanie_siodme():
+#     n = int(input("Zadanie 7. Podaj liczbe: "))
+#
+#     for i in range(1, n+1):
+#         if test_pierwszej(n):
+#             print(i)
+
 
 
 __name__ = main()
