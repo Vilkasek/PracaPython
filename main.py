89c0473352c8fd10fb30cb6ab71d979abe45228f
Vilkasek <jakub.brandt2002@gmail.com>
2022-11-02 13:12:47 +0100

    Zaczecie zadania 5.

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ def main():
 
     zadanie_trzecie_for(n, suma)
     zadanie_trzecie_while(n, suma)
+    zadanie_czwarte(n, suma)
 
 def zadanie_pierwsze(name):
     print("Zadanie pierwsze:\n")
@@ -29,7 +30,8 @@ def zadanie_trzecie_for(n, suma):
     for i in range(1, n+1):
         suma = suma + i
     
-    print("Zadanie trzecie:\n", suma)
+    print("Zadanie trzecie:")
+    print(suma)
 
 def zadanie_trzecie_while(n, suma):
     i = 1
@@ -40,5 +42,17 @@ def zadanie_trzecie_while(n, suma):
         i = i + 1
 
     print(suma)
+
+def zadanie_czwarte(n, suma):
+    for i in range(1, n+1):
+        if i % 3 == 0 or i % 5 == 0:
+            suma = suma + i
+        else:
+            continue
+
+    print(suma)
+
+def zadanie_piate():
+    pass
 
 __name__ = main()
