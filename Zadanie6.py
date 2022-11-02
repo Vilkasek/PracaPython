c1a93d2f6cf34222d502a2f4e0b676ad710aac5a
Vilkasek <jakub.brandt2002@gmail.com>
2022-11-02 14:47:02 +0100

    Dodanie zadania 6.

---
+++ Zadanie6.py
@@ -0,0 +1,8 @@
+i = 0
+
+while i <= 12:
+    for j in range(0, 12+1):
+        multi = j * i
+        print(i, "*", j, "to", multi)
+
+    i += 1
--- main.py
+++
@@ -1,9 +0,0 @@
-# def zadanie_szoste():
-#     pass
-#
-# def zadanie_siodme():
-#     n = int(input("Zadanie 7. Podaj liczbe: "))
-#
-#     for i in range(1, n+1):
-#         if test_pierwszej(n):
-#             print(i)
