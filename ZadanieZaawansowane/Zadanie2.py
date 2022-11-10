cd4a398f0abb679d7101b5d24f710d53a3ece4cb
Vilkasek <jakub.brandt2002@gmail.com>
2022-11-10 08:48:07 +0100

    Ukończenie zaawansowanego zadania 2

---
+++ ZadanieZaawansowane/Zadanie2.py
@@ -0,0 +1,19 @@
+def palindrome(n):
+    n = n.upper()
+
+    i = len(n)
+    i = int(i)
+    while i:
+        i -= 1
+        if n[i] != n[-i-1]: return False
+    return True
+
+def main():
+    word = str(input("Podaj słowo, które chcesz sprawdzić, czy jest palindromem: "))
+
+    if palindrome(word):
+        print("Tak to jest palindrom")
+    else:
+        print("To nie jest palindrom")
+
+main()
